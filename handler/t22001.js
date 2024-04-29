a4b4f7364c7e772e0c43cb432a4c31eebfcaf972
anshulmann11 <anshulmann012@gmail.com>
2024-04-30 01:49:55 +0530

    added pusblish messages through rabbit mq

--- handler/t22001.js
+++ handler/t22001.js
@@ -1,5 +1,20 @@
-function t22001(clientInfo) {
+const DeviceDataPublisher = require("../lib/rabbitMq/deviceDataPublisher");
+
+async function t22001(clientInfo) {
   console.log("coming her inseide");
+  const deviceId = "12345";
+  const data = {
+    data: "extracted data",
+  };
+
+  try {
+    const queueName = "device_data_queue";
+    const publisher = new DeviceDataPublisher(queueName);
+    await publisher.publishData(deviceId, data);
+    console.log("Device data published to queue successfully");
+  } catch (error) {
+    console.error("Error publishing device data:", error);
+  }
 }
 
 module.exports = { t22001 };
